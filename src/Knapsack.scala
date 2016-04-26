21fa730bf77a11c297a6f666e926d6331d96bbfc
haffla <jakob.pupke@googlemail.com>
2016-04-26 23:04:38 +0200

    inital commit

---
+++ src/Knapsack.scala
@@ -0,0 +1,21 @@
+object Knapsack {
+
+  def main(args: Array[String]) {
+    val res = knapsack(List(1,2,5,9))
+    res.foreach(println(_))
+    getSolution(res, 16) match {
+      case Some(s) => println("Solution: " + s)
+      case _ => println("No Solution!")
+    }
+  }
+
+  def knapsack[T](numbers: List[T]): List[List[T]] = {
+    numbers match {
+      case Nil => List(Nil)
+      case head ::  Nil => List(head) :: List(Nil)
+      case head :: tail => knapsack(tail) ++ knapsack(tail).map(l => head :: l)
+    }
+  }
+
+  def getSolution[T:Numeric](solutions: List[List[T]], sum: T): Option[List[T]] = solutions.find(_.sum == sum)
+}
