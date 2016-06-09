276b867e715a32368c7c511cfb5ae19d4e0abce6
Jakob Pupke <jakob.pupke@googlemail.com>
2016-06-09 12:48:56 +0200

    bino fix

--- src/week1/Binomial.scala
+++ src/week1/Binomial.scala
@@ -2,11 +2,11 @@ package week1
 
 object Binomial {
   def main(args: Array[String]): Unit = {
-    println(bino(6,2))
+    println(bino(0,2))
   }
 
   def bino(n:Int, k:Int): Int = {
-    if(k == 0) 1
+    if(k == 0 || n == 0) 1
     else if(k == n) 1
     else
       bino(n-1, k-1) + bino(n-1, k)
